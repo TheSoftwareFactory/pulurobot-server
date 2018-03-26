994e0251243528c4c01ff0fcd289681d07bd41ad
Gabriel Gatu <gabriel.dny@gmail.com>
2018-03-26 15:00:56 +0300

    Create a db pool for managing connections

---
+++ src/db/mod.rs
@@ -0,0 +1,28 @@
+extern crate lazy_static;
+
+extern crate r2d2;
+extern crate r2d2_sqlite;
+extern crate rusqlite;
+
+pub mod robot;
+
+use std::path::Path;
+use std::env;
+use self::r2d2_sqlite::SqliteConnectionManager;
+
+lazy_static! {
+    pub static ref DB_POOL: r2d2::Pool<r2d2_sqlite::SqliteConnectionManager> = setup_db_pool();
+}
+
+fn setup_db_pool() -> r2d2::Pool<r2d2_sqlite::SqliteConnectionManager> {
+    let path = env::current_dir()
+            .unwrap()
+            .join(Path::new("db/database.db"));
+    let manager = SqliteConnectionManager::file(path);
+    r2d2::Pool::new(manager).unwrap()
+}
+
+pub fn get_connection() -> r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager> {
+    let pool = DB_POOL.clone();
+    pool.get().unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[macro_use]
+extern crate lazy_static;
+
 extern crate rocket;
 extern crate ws;
 
