18f8fff78dbf38005735af8894bd1c207734653e
Gabriel Gatu <gabriel.dny@gmail.com>
2018-05-08 19:32:24 +0300

    Send also the name of the event when dispatching

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -1,4 +1,5 @@
 use ws::Sender;
+use serde_json::{self, Value};
 use std::sync::Mutex;
 use std::collections::HashMap;
 
@@ -24,11 +25,30 @@ impl Dispatcher {
     }
 
     pub fn publish_event(event: &str, message: String) {
-        let mut dispatcher = DISPATCHER.lock().unwrap();
+        let dispatcher = DISPATCHER.lock().unwrap();
         if let Some(subscribers) = dispatcher.subscribers.get(event) {
             for subscriber in subscribers {
-                subscriber.send(message.clone());
+                let json = Event::to_json(event.to_string(), message.clone());
+                subscriber.send(json);
             }
         }
+    }
+}
+
+#[derive(Serialize)]
+struct Event {
+    event: String,
+    payload: Value,
+}
+
+impl Event {
+    fn to_json(event: String, payload: String) -> String {
+        let v: Value = serde_json::from_str(&payload).unwrap();
+
+        let object = Event{
+            event: event, 
+            payload: v
+        };
+        serde_json::to_string(&object).unwrap()
     }
 }
